842f28a7e523495fb9610b1fbaff89764120133b
pokotaliu <liupokopan@gmail.com>
2025-07-14 10:43:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const config = {
   type: Phaser.AUTO,
-  width: 640,
-  height: 480,
+  width: window.innerWidth - 260, // 扣掉右側訊息框寬度
+  height: window.innerHeight,
   backgroundColor: '#1d1d1d',
   scene: {
     preload,
@@ -11,21 +11,27 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+
+let scene;
 let pokota;
 
 function preload() {
+  // 暫時用 dummy image 模擬胖波角色
   this.load.image('pokota', 'https://dummyimage.com/32x32/ff69b4/000000&text=P');
 }
 
 function create() {
-  window.scene = this;
+  scene = this;
+  window.scene = scene;
 
-  pokota = this.add.sprite(320, 240, 'pokota');
+  // 建立胖波角色
+  pokota = this.add.sprite(200, 200, 'pokota');
   window.pokota = pokota;
 
-  showMessage('歡迎來到胖波測試場～');
+  // 初始訊息
+  showMessage('🎮 遊戲啟動，胖波出場啦！');
 }
 
 function update() {
-  // 可擴充邏輯
+  // 可擴充邏輯，例如自動移動、碰撞檢查等
 }
